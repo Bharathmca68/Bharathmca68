124cb1e881a2dcd7c29a2ac8f86ef8f0f5c17bde
Bharath M <80087884+Bharathmca68@users.noreply.github.com>
2023-10-07 12:12:03 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -203,7 +203,7 @@ var Tawk_API = Tawk_API || {},
   var s1 = document.createElement("script"),
     s0 = document.getElementsByTagName("script")[0];
   s1.async = true;
-  s1.src = "https://embed.tawk.to/60df10bf7f4b000ac03ab6a8/1f9jlirg6";
+  s1.src='https://embed.tawk.to/6520fbaceb150b3fb99ef6ed/1hc4de7ji';
   s1.charset = "UTF-8";
   s1.setAttribute("crossorigin", "*");
   s0.parentNode.insertBefore(s1, s0);
